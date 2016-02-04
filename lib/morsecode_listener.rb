8d8b2f5430f02b2d70e0337440e02d232813dc61
Your Name <you@example.com>
2016-02-04 18:10:12 +0000

    feature: A space now be created betwen words.

--- lib/morsecode_listener.rb
+++ lib/morsecode_listener.rb
@@ -5,10 +5,10 @@
 class MorseCodeListener
 
   def initialize(short_delay: 0.35, long_delay: 1, dot: 2, dash: 1, 
-        separator: 4, timeout: 2, notifier: nil)
+        separator: 4, space: 5, timeout: 2, notifier: nil)
 
     @short_delay, @long_delay, @timeout = short_delay, long_delay, timeout
-    @h = { dot: dot, dash: dash, separator: separator }
+    @h = { dot: dot, dash: dash, separator: separator, space: space }
     @notifier = notifier
     @t, @a = nil, []
   end
@@ -35,16 +35,17 @@ class MorseCodeListener
       sleep @short_delay * 2
       @a << @h[:separator]
       sleep @long_delay - @short_delay
-      @a << @h[:separator]
+      @a.pop
+      @a << @h[:space]
       sleep @timeout - @long_delay - @short_delay
-      @a.pop 2
+      @a.pop
 
       s = @a.join
 
       if @notifier then
 
         begin
-          @notifier.message s
+          @notifier.message s if @notifier.respond_to? :message
         rescue
           puts 'warning: MorseCodeListener#on_keyup @notifier' + ($!).inspect
         end
